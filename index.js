25cf35661e671f1003f8568be11ff4035eddabba
Batoul El Mansour <elmansourbatoul@gmail.com>
2022-11-28 13:05:41 +0200

    done

--- index.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById('button');
-
-function clickAlert() {
-  alert('I was clicked!');
-}
-
-function addingEventListener() {
-input.addEventListener('click', clickAlert);
-}
